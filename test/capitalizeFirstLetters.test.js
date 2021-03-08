d61987c0854f77b156a54142b83e0a7532db3537
Edouard Misset <edouardmisset@gmail.com>
2021-03-08 10:14:44 +0100

    refactored using mocha

--- test/capitalizeFirstLetters.test.js
+++ test/capitalizeFirstLetters.test.js
@@ -4,17 +4,31 @@ import { capitalizeFirstLetters } from '../capitalizeFirstLetters.js';
 
 // Tests:
 
-// Check that capitalizeFirstLetters is a function
-strictEqual(typeof capitalizeFirstLetters, 'function');
+// Gives the test suite a label using describe
+describe('capitalizeFirstLetters', () => {
+  // Gives the test a label using it
+  it('is a function accepting one argument', () => {
+    // Checks that capitalizeFirstLetters is a function
+    strictEqual(typeof capitalizeFirstLetters, 'function');
+    // Checks that capitalizeFirstLetters accepts one argument
+    strictEqual(capitalizeFirstLetters.length, 1);
+  });
 
-// Check that capitalizeFirstLetters accepts one argument
-strictEqual(capitalizeFirstLetters.length, 1);
+  it('transforms a string correctly', () => {
+    // Checks that capitalizeFirstLetters transforms javaScript correctly
+    strictEqual(
+      capitalizeFirstLetters('i am learning TDD'),
+      'I Am Learning TDD'
+    );
+  });
 
-// Check that capitalizeFirstLetters transforms javaScript correctly
-strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
+  it('works with a 1-char string', () => {
+    // Checks that it works for a 1-character string
+    strictEqual(capitalizeFirstLetters('x'), 'X');
+  });
 
-// Check that it works for a 1-character string
-strictEqual(capitalizeFirstLetters('x'), 'X');
-
-// Check that it works for an empty string
-strictEqual(capitalizeFirstLetters(''), '');
+  it('works with an empty string', () => {
+    // Checks that it works for an empty string
+    strictEqual(capitalizeFirstLetters(''), '');
+  });
+});
